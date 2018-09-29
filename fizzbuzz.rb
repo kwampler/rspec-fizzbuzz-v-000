ac7800c73d761da67fb8d6c6b25fd91207e011f9
katejsenger <katejsenger@yahoo.com>
2018-09-29 20:53:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(int)
-if int % 3 == 0 && int % 5 == 0
-  puts "fizzbuzz"
-elsif int % 3 == 0
-  puts "Fizz"
-elsif int % 5 == 0
-  puts "Buzz"
-else 
-  puts "nil"
+  if int % 3 == 0 && int % 5 == 0
+    puts "fizzbuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else 
+    puts "nil"
+  end
 end
